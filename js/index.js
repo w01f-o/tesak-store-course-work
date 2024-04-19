93dfbdc24d306cdc54f8cb21788977b0ee7d3d31
w01f <arsenimarchenko@bk.ru>
2024-04-20 01:17:42 +0300

    .

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".btn-next",
+    prevEl: ".btn-prev",
+  },
+});
