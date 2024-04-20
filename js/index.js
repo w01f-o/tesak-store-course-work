4ab6071307b93d038d907eb9e3c6ff371131ed5f
w01f <arsenimarchenko@bk.ru>
2024-04-20 22:56:36 +0300

    adaptive

--- js/index.js
+++ js/index.js
@@ -9,3 +9,18 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".btn-prev",
   },
 });
+
+const burgerButton = document.querySelector('.burger__button');
+const burgerMenu = document.querySelector('.burger__menu');
+
+burgerButton.addEventListener('click', () => {
+  burgerMenu.classList.toggle('burger__menu_open');
+  document.body.classList.toggle('body_backdrop');
+})
+
+document.body.addEventListener('click', (e) => {
+  if (!e.target.matches('.burger__button')) {
+    burgerMenu.classList.remove('burger__menu_open');
+    document.body.classList.remove('body_backdrop');
+  }
+})
